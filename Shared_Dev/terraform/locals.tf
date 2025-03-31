0fa0a4fe3a1a7601803362ddae23494cb410e90c
vikas <dvikas793@gmail.com>
2025-03-31 13:12:32 +0530

    shared_dev

--- Shared_Dev/terraform/locals.tf
+++ Shared_Dev/terraform/locals.tf
@@ -34,7 +34,7 @@ locals {
   private_subnet_rtb_name_db = "rtb-shared-dev-db-private-mum-01"
  
  
-  network_account_id = module.aft_account_list.param_name_values["${local.ssm_parameter_path_account_list}account-lz2-network"]
+  network_account_id = network_account_id = module.aft_account_list.param_name_values["${local.ssm_parameter_path_account_list}account-lz2-network"]
   network_tgw_id     = data.aws_ec2_transit_gateway.primary_network_tgw.id
 
   
